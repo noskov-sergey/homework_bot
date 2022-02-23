2bc8df23eecabe6e236b5cb452c810bc2d8d86ad
Noskov Sergey <noskov.sergey@mail.ru>
2022-02-23 22:02:10 +0500

    начало работы

--- homework.py
+++ homework.py
@@ -1,16 +1,22 @@
-...
+import logging
+import os
+import time
+
+from telegram import Bot
+import telegram
+import requests
+from telegram.ext import CommandHandler, Updater
+from dotenv import load_dotenv
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
 
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -18,9 +24,14 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
 
 def send_message(bot, message):
-    ...
+    """Отправляет сообщение в Telegram чат."""
+    bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(current_timestamp):
@@ -48,13 +59,19 @@ def parse_status(homework):
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
-def check_tokens():
-    ...
+def check_tokens(PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID):
+    if PRACTICUM_TOKEN and TELEGRAM_TOKEN or TELEGRAM_CHAT_ID is None:
+        return False
+    else:
+        return True
 
 
 def main():
     """Основная логика работы бота."""
-
+    print(check_tokens(PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
+    print(PRACTICUM_TOKEN)
+    print(TELEGRAM_TOKEN)
+    print(TELEGRAM_CHAT_ID)
     ...
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
@@ -81,3 +98,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+print(check_tokens)
